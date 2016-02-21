ed4d4b2e46f90f31ce8f5bc8f688919e213520b7
Anirudha Bose <ani07nov@gmail.com>
2016-02-21 23:44:05 +0530

    Switch to Executor class in UNIXBackend

--- core/backends/UNIX.py
+++ core/backends/UNIX.py
@@ -1,13 +1,14 @@
 import os
 
 from core.backends.IUnikernelBackend import IUnikernelBackend
-from core.utils.executor import logged_call, call
+from core.utils.Executor import Executor
 from core.api.settings import _Config as Config  # TODO: Need to switch to proper config (Dev / Prod)
 
 
 class UNIXBackend(IUnikernelBackend):
     def __init__(self):
         self.work_dir = None
+        self.executor = None
 
     def register(self, project, module, _id, config, unikernel):
         self.work_dir = os.path.join(
@@ -18,6 +19,8 @@ class UNIXBackend(IUnikernelBackend):
             ),
             str(_id)
         )
+
+        self.executor = Executor(self.work_dir)
 
         os.makedirs(
             self.work_dir,
@@ -44,15 +47,15 @@ class UNIXBackend(IUnikernelBackend):
 
     def configure(self, _id):
         # TODO: Need better exception handling
-        logged_call('mirage configure --unix', self.work_dir)
+        self.executor.logged_call('mirage configure --unix')
 
     def compile(self, _id):
         # TODO: Need better exception handling
-        logged_call('make', self.work_dir)
+        self.executor.logged_call('make')
 
     def optimize(self, _id):
-        call('strip main.native', self.work_dir)
+        self.executor.logged_call('strip main.native')
 
     def start(self, _id):
         # FIXME: This call needs to be asynchronous
-        logged_call('main.native', self.work_dir)
+        self.executor.logged_call('./main.native')
