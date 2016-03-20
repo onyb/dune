034524b341a461d202678aa1dac8a78f7834006f
Anirudha Bose <ani07nov@gmail.com>
2016-03-21 02:37:55 +0530

    Integrate AlertsCtrl with CreateCtrl controller

--- client/src/js/controllers/create-ctrl.js
+++ client/src/js/controllers/create-ctrl.js
@@ -20,9 +20,9 @@ function CreateCtrl($scope, $http) {
         };
 
         $http.post("http://localhost:5000/api/unikernel/create", payload).success(function(data, status) {
-            alert(JSON.stringify(data));
+            $scope.addAlert('success', '[code ' + data.code + '] [ID ' + data._id + '] ' + data.message);
         }).error(function(data, status) {
-            alert(JSON.stringify(data));
+            $scope.addAlert('danger', '[code ' + data.code + '] ' + data.message);
         });
     };
 }
